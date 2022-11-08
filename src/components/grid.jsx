6c9832d16ae37c2fbfe263a966fbd59f85df2907
balesniy <balesniy@yandex.ru>
2022-11-08 16:43:54 +0000

    Update 13 files

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,56 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Card from './card';
 
-const TIMEOUT = 600;
-
 function Grid({
   images = [],
-  finishedItems,
-  checkItems,
+  finishedItems = [],
 }) {
-  const [
-    visibleItems,
-    setVisibleItems,
-  ] = useState([]);
-
-  const handleCardClick = (index) => {
-    if (
-      finishedItems.includes(index) ||
-      visibleItems.includes(index)
-    ) {
-      return;
-    }
-
-    switch (visibleItems.length) {
-      case 0:
-        // we need intermediate case without timer
-        // case 2:
-        setVisibleItems([index]);
-        break;
-      case 1:
-        setVisibleItems((items) => [
-          ...items,
-          index,
-        ]);
-        checkItems(
-          visibleItems[0],
-          index
-        );
-        // You don’t need Effects to handle user events.
-        setTimeout(() => {
-          setVisibleItems([]);
-        }, TIMEOUT);
-        break;
-      default:
-        setVisibleItems([]);
-    }
-  };
-
-  // fixme after markup changes
-  const columnsCount =
-    images.length === 12 ? 3 : 4;
-
   return (
     <div className="container">
       <div className="row no-gutters">
@@ -58,20 +13,9 @@ function Grid({
           <Card
             key={item.id}
             item={item}
-            visible={visibleItems.includes(
+            isFinished={finishedItems.includes(
               index
             )}
-            finished={finishedItems.includes(
-              index
-            )}
-            className={`${
-              columnsCount === 3
-                ? 'col-4'
-                : 'col-3'
-            }`}
-            onCardClick={() =>
-              handleCardClick(index)
-            }
           />
         ))}
       </div>
